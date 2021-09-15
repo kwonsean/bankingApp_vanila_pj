00421c1535d89f31887686ea0fd7fa619820a2d0
kwonsean <gownn12@naver.com>
2021-09-15 15:04:15 +0900

    refactor: Range feat refactoring
    range의 결과 값이 페이지가 서로 연동되게하고 조작은 page2에서만
    가능하도록 리팩토링

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
 // range관련 시작 
 // input 태그를 긁어 오기
 const inputs = document.querySelectorAll('input[type="range"]')
-const input = document.querySelector('.range_wrap > input')
+const inputPage1 = inputs[0];
+const inputPage2 = inputs[1];
+let style = document.querySelector('[data="test"]');
 
-
+function setData(x){
+  style.innerHTML = "input.range::-webkit-slider-thumb { left: " + (x-50) + "%;} .set_budget input.range::-webkit-slider-thumb {left:0px !important;}";
+}
 // 실행할 내용
 function inputUpdate () {
   // 넘어오는 이벤트 주체의 값 (range의 경우이므로 0~100(%) 중 하나의 값)
   const perVal = this.value
-  // console.log(perVal)
+  setData(this.value)
  
   // slider-thumb(컨트롤 하는 버튼)의 값을 기준으로 전후 그라디언트
   // input의 배경색을 그라디언트로 해서 버튼을 기준으로 오른쪽은 색 왼쪽은 색 X 
   this.style.background = `linear-gradient(to right, #FFDB4C 0%, #FFDB4C ${perVal}%, #C4C4C4 ${perVal}%, #C4C4C4 100%)`
+  inputPage1.style.background = `linear-gradient(to right, #FFDB4C 0%, #FFDB4C ${perVal}%, #C4C4C4 ${perVal}%, #C4C4C4 100%)`
   // 오른쪽부터 #FFDB4C(노랑색) 0%부터 현재 버튼의 (사실은 마우스 or 터치한 부분)의 위치%만큼 채우고 그 위치부터 배경색인 #C4C4C4(회색)을 끝까지 채운다.
 }
 
-// 공통
-inputs.forEach(input => input.addEventListener('change', inputUpdate))
-// 웹
-inputs.forEach(input => input.addEventListener('mousemove', inputUpdate))
-// 모바일
-inputs.forEach(input => input.addEventListener('touchmove', inputUpdate))
+inputPage2.addEventListener('change', inputUpdate);
+inputPage2.addEventListener('mousemove', inputUpdate);
+inputPage2.addEventListener('touchmove', inputUpdate);
 
 // range에 초기값 지정
-function fristSet(){
-  input.style.background = `linear-gradient(to right, #FFDB4C 0%, #FFDB4C 50%, #C4C4C4 50%, #C4C4C4 100%)`
-}
-fristSet();
+(function firstset(){
+  inputPage1.style.background = `linear-gradient(to right, #FFDB4C 0%, #FFDB4C 50%, #C4C4C4 50%, #C4C4C4 100%)`
+  inputPage2.style.background = `linear-gradient(to right, #FFDB4C 0%, #FFDB4C 50%, #C4C4C4 50%, #C4C4C4 100%)`
+}());
 
 // range관련 끝
 
@@ -75,6 +77,9 @@ function clickCloseBtn(){
 }
 chartBtn.addEventListener('click', clickChartBtn)
 closeBtn.addEventListener('click', clickCloseBtn)
+// 2페이지 나타내기 끝
+
+
 // 막대그래프 시작
 const data = {
   labels: [
